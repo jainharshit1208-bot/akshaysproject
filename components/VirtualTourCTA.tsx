75adc063523bb21edafdbc0ed046b8373dff1179
dungeonme <ak64ma@gmail.com>
2026-02-08 03:25:05 +0530

    Fix video source path in VirtualTourCTA component

--- components/VirtualTourCTA.tsx
+++ components/VirtualTourCTA.tsx
@@ -60,7 +60,7 @@ const VirtualTourCTA: React.FC = () => {
             className="w-full h-full object-cover"
             onEnded={() => setIsPlaying(false)}
           >
-            <source src="/video.mp4" type="video/mp4" />
+            <source src="video.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
